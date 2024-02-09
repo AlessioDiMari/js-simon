d34cc02fec19583e90fbac40630d7aa6104e5803
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-09 16:32:37 +0100

    creo la variavile orario

--- jsc/script.js
+++ jsc/script.js
@@ -5,10 +5,14 @@ Ogni secondo il nostro countdown dovrà
 scalare fino alle 9:30 di lunedì mattina!
 */
 
-// Setto la fine del timer a sabato 10/02/2024 alle 9:30
+// Setto la fine del timer a sabato 10/02/2024
 let dataFine = new Date();
 
 dataFine.setDate(dataFine.getDate() + 1)
+
+// Setto anche l'orario
+dataFine.setHours(9);
+dataFine.setMinutes(30);
 
 
 // Faccio la formattazione della data in "giorno"/"mese"/"anno"
@@ -17,7 +21,15 @@ let giorno = dataFine.getDate();
 let mese = dataFine.getMonth() + 1;
 let anno = dataFine.getFullYear();
 
-// Creo la srtinga della data
+// Faccio la formattazione dell'orario
+let ore = dataFine.getHours();
+let minuti = dataFine.getMinutes();
+
+
+// Creo la stringa della data
 let dataScritta = giorno + "/" + mese + "/" + anno;
+
+// Creo la stringa dell'orario
+let orarioScritto = ore + ":" + minuti;
 
 
