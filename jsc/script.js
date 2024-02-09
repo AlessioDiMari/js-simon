426004e36006abfc433d6511ee42176cb7ff2c39
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-09 16:19:18 +0100

    setto la data di domani

---
+++ jsc/script.js
@@ -0,0 +1,13 @@
+/*
+Sfruttiamo le timing functions per fare il
+conto alla rovescia per la correzione di domani!
+Ogni secondo il nostro countdown dovrà
+scalare fino alle 9:30 di lunedì mattina!
+*/
+
+// Setto la fine del timer a sabato 10/02/2024 alle 9:30
+let dataFine = new Date();
+
+dataFine.setDate(dataFine.getDate() + 1)
+
+
